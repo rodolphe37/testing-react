16f6e6ec73164d765f63d5a02b7cbc5f95a07711
rodolphe37 <rodolphe.a@gmail.com>
2022-05-24 21:06:34 +0200

    added wdio specs

--- test/specs/visual.spec.js
+++ test/specs/visual.spec.js
@@ -2,7 +2,7 @@ describe("My React application", () => {
   it("should look correct", () => {
     browser.url("/");
     browser.execute("/*@visual.init*/", "React App");
-    // browser.execute("/*@visual.snapshot*/", "Home Page");
+    browser.execute("/*@visual.snapshot*/", "Home Page");
     const result = browser.execute("/*@visual.end*/");
     expect(result.message).toBeNull();
   });
